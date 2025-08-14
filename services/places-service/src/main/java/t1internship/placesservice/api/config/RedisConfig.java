ab219319912f76c9bb9f46d8823d1b4ee65627d5
raimurzin <aimurzin05@mail.ru>
2025-08-15 00:19:29 +0500

    written logging

--- services/places-service/src/main/java/t1internship/placesservice/api/config/RedisConfig.java
+++ services/places-service/src/main/java/t1internship/placesservice/api/config/RedisConfig.java
@@ -3,13 +3,27 @@ package t1internship.placesservice.api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-
 @Configuration
 public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName("10.10.146.230");
+        config.setPort(6379);
+        config.setPassword("foobared"); // Удалите, если пароль не используется
+        
+        // Для просмотра параметров (при необходимости)
+        System.out.println("Redis Config: " + config);
+        
+        return new LettuceConnectionFactory(config);
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
@@ -19,5 +33,4 @@ public class RedisConfig {
         template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
         return template;
     }
-
 }
