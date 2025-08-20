501a0dd3bf8f95fc1df6c4c4394f796a772c701e
portner-boop <damirmagdeev1054@gmail.com>
2025-08-20 11:47:17 +0400

    prod-config-fix-visualization-service

--- services/visualization-service/src/main/resources/db.migration/V1_create_visualization_schema.sql
+++ services/visualization-service/src/main/resources/db.migration/V1_create_visualization_schema.sql
@@ -1,14 +1,12 @@
 CREATE SCHEMA IF NOT EXISTS visualization_schema;
 
-
-
-CREATE TABLE IF NOT EXISTS floor_visualization (
+CREATE TABLE IF NOT EXISTS visualization_schema.floor_visualization (
 id BIGSERIAL PRIMARY KEY,
 floor_id BIGINT NOT NULL,
 image_url VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS space_coordinates (
+CREATE TABLE IF NOT EXISTS visualization_schema.space_coordinates (
  id BIGSERIAL PRIMARY KEY,
  space_id BIGINT NOT NULL,
  x INTEGER NOT NULL,
@@ -19,7 +17,3 @@ CREATE TABLE IF NOT EXISTS space_coordinates (
          REFERENCES floor_visualization(id)
          ON DELETE CASCADE
 );
-
-CREATE INDEX IF NOT EXISTS idx_floor_visualization_floor_id ON floor_visualization(floor_id);
-CREATE INDEX IF NOT EXISTS idx_space_coordinates_space_id ON space_coordinates(space_id);
-CREATE INDEX IF NOT EXISTS idx_space_coordinates_floor_image_id ON space_coordinates(floor_image_id);
