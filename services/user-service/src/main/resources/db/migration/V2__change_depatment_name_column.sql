c48342c1f087353817ea8ceb0684e96d5984460a
VladimirShatrov <vova.shatrov.2005@gmail.com>
2025-08-20 13:21:57 +0500

    fix

--- services/user-service/src/main/resources/db/migration/V2__change_depatment_name_column.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE profile_schema.departments RENAME COLUMN name To title;
-
-ALTER TABLE profile_schema.departments ADD CONSTRAINT departments_title_unique UNIQUE (title);
-
-ALTER TABLE profile_schema.departments DROP CONSTRAINT IF EXISTS departments_name_unique;
