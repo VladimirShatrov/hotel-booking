12debb60bac60ab46a8f324987de78aa75492e39
VladimirShatrov <vova.shatrov.2005@gmail.com>
2025-09-09 17:32:28 +0500

    first deleting

--- services/visualization-service/src/main/java/t1internship/visualizationservice/api/exception/NotFoundFloorException.java
+++
@@ -1,7 +0,0 @@
-package t1internship.visualizationservice.api.exception;
-
-public class NotFoundFloorException extends RuntimeException {
-    public NotFoundFloorException(String s) {
-        super(s);
-    }
-}
--- services/visualization-service/src/main/java/t1internship/visualizationservice/api/exception/NotFoundSpaceException.java
+++
@@ -1,7 +0,0 @@
-package t1internship.visualizationservice.api.exception;
-
-public class NotFoundSpaceException extends RuntimeException {
-    public NotFoundSpaceException(String s) {
-        super(s);
-    }
-}
